293207b133953ab26cedfcb68f1cb7675c577126
Kacper Malachowski <kacpermaachowski@gmail.com>
2022-10-04 10:49:52 +0200

    Add _document with gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RZ88HMHSMW"></script>
-        <script>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RZ88HMHSMW"
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'G-RZ88HMHSMW');
-        </script>
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
