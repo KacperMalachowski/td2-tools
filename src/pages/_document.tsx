53be224a215d8f43ab378c7c36dc1597a9cc9b7d
Kacper Małachowski <38684517+KacperMalachowski@users.noreply.github.com>
2022-10-04 10:35:52 +0200

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RZ88HMHSMW"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-RZ88HMHSMW');
+        </script>
+      </Head>
       <body>
         <Main />
         <NextScript />
